c738f996309903fb05929c2accc469bdf78add24
LevDm <71178328+LevDm@users.noreply.github.com>
2023-03-02 01:55:43 +0500

    blanks

--- screens_navigater/app_pages/notes/Notes.js
+++ screens_navigater/app_pages/notes/Notes.js
@@ -1,140 +1,75 @@
 import React, {useState, useEffect, useRef, useMemo} from "react";
 
 import {
-    //Text, 
-    Pressable, 
-    Animated, 
-    View,
-    Button, 
-    Platform, 
-    Image, 
-    Dimensions, 
-    FlatList, 
-    SafeAreaView, 
-    TouchableOpacity, 
-    StyleSheet
+  Text,
+  Pressable, 
+  View,
+  Dimensions, 
+  FlatList, 
+  StyleSheet,
+  useWindowDimensions
 } from 'react-native';
 
 import {
-  BackdropBlur,
-  Canvas,
-  rect,
-  Fill,
-  rrect,
-  vec,
   useValue,
-  useTouchHandler,
-  Rect,
-  LinearGradient,
-  Paint,
-  Text,
   useComputedValue,
-  runDecay,
-  useFont,
+  Shadow,
+  Canvas,
+  rect, 
+  rrect,
+  Fill,
   Group,
+  Paint,
+  RoundedRect,
 } from "@shopify/react-native-skia";
 
-import { useWindowDimensions } from "react-native";
 
-import { Background } from "./Background";
-import { Ball } from "./Ball";
-
-//const titleFont = useFont(require("./SF-Mono-Semibold.otf"), 24);
-//const subtitleFont = useFont(require("./SF-Mono-Semibold.otf"), 18);
-//const font = useFont(require("./SF-Mono-Semibold.otf"), 12);
-const height = Dimensions.get('window').height
-const width = Dimensions.get('window').width
-const CARD_WIDTH = width - 64;
-const CARD_HEIGHT = CARD_WIDTH * 0.61;
-
-export const Glassmorphism = () => {
-  const titleFont = useFont(require("./SF-Mono-Semibold.otf"), 24);
-  const subtitleFont = useFont(require("./SF-Mono-Semibold.otf"), 18);
-  const font = useFont(require("./SF-Mono-Semibold.otf"), 12);
-  //const { width, height } = useWindowDimensions();
+const Neumorphism = () => {
   
-  const clip = useMemo(
-    () => rrect(rect(0, 0, CARD_WIDTH, CARD_HEIGHT), 20, 20),
-    [CARD_HEIGHT, CARD_WIDTH]
-  );
-
-  const x = useValue((width - CARD_WIDTH) / 2);
-  const y = useValue((height - CARD_HEIGHT) / 2);
-  const offsetX = useValue(0);
-  const offsetY = useValue(0);
-  const onTouch = useTouchHandler({
-    onStart: (pos) => {
-      offsetX.current = x.current - pos.x;
-      offsetY.current = y.current - pos.y;
-    },
-    onActive: (pos) => {
-      x.current = offsetX.current + pos.x;
-      y.current = offsetY.current + pos.y;
-    },
-    onEnd: ({ velocityX, velocityY }) => {
-      runDecay(x, { velocity: velocityX });
-      runDecay(y, { velocity: velocityY });
-    },
-  });
-  const transform = useComputedValue(
-    () => [{ translateY: y.current }, { translateX: x.current }],
-    [x, y]
-  );
-  if (titleFont === null || subtitleFont === null || font === null) {
-    return null;
-  }
+  const FULL_SIZE = 256
+  const PADDING = 10;
+  const SIZE = FULL_SIZE - 2*PADDING;
+  const R = 16;
+  //<Fill color="lightblue" />width={'100%'} height={SIZE}      
+  const size = useValue({ width: FULL_SIZE, height: FULL_SIZE });
+  const rct = useComputedValue(() => {
+    //console.log('s?', size.current)
+    return rrect(rect(PADDING, PADDING, (size.current.width - 2*PADDING), (size.current.height - 2*PADDING)), R, R);
+  }, [size]);
   return (
-    <Canvas style={{ flex: 1 }} onTouch={onTouch} debug>
-      <Fill>
-      <LinearGradient
-          start={vec(0, 0)}
-          end={vec(0, height)}//rect={rect(0, 0, width, height)}
-          colors={["#FAC6C0", "#EBBFF6", "#F5DFE6", "#F2DCF6"]}
-      />
-      </Fill>
-      <Ball r={100} c={vec(375, 75)} />
-      <Ball r={50} c={vec(width, height / 2)} />
-      <Ball r={100} c={vec(150, height - 200)} />
-      <Ball r={75} c={vec(300, height / 2 - 100)} />
-
-      <BackdropBlur
-        clip={clip}
-        blur={12}
-      
-        
-        transform={transform}
+    <Canvas
+      onSize={size}
+      //onLayout={(event)=>{
+      //  console.log(event.nativeEvent.layout)
+      //}}
+      style={{height: '100%', width: '100%', position: 'absolute'}} 
+    >
+      <RoundedRect 
+        rect={rct}
+        color="lightblue"
       >
-        <Fill color="rgba(255, 255, 255, 0.1)"/>
-
-          <LinearGradient
-            start={vec(0, 0)}
-            end={vec(CARD_WIDTH, 0)}
-            colors={["#5DA7D2ee", "#B848D9ee"]}
-          />
-
-        <Rect x={0} y={CARD_HEIGHT - 70} width={CARD_WIDTH} height={70} />
-        <Text text="SUPERBANK" x={20} y={40} font={titleFont} />
-        <Text x={20} y={110} text="1234 5678 1234 5678" font={titleFont} />
-        <Text text="VALID THRU" x={20} y={145} color="white" font={font} />
-        <Text text="12/29" x={20} y={160} color="white" font={font} />
-        <Text
-          text="JOHN DOE"
-          x={20}
-          y={185}
-          color="white"
-          font={subtitleFont}
-        />
-      </BackdropBlur>
+        <Shadow dx={PADDING/2.6} dy={PADDING/2.6} blur={PADDING/3.5} color="#00000020" />
+        <Shadow dx={-PADDING/2.6} dy={-PADDING/2.6} blur={PADDING/3.5} color="#ffffff35" />
+      </RoundedRect>
     </Canvas>
   );
 };
 
 export default Notes = () => {
-
-
+  //<Neumorphism />
   return (
-    <View style = {{ flex: 1}}>
-      <Glassmorphism />
+    <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightblue'}}>
+      
+     
+      <View style={{height: 100, width: 200, justifyContent: 'center', alignItems: 'center',}}>
+        <Neumorphism />
+        <Text>
+          this my view
+        </Text>
+      </View>
+        
+
+      
     </View>
   );
     
---
+++ screens_navigater/app_pages/settings_stack/settings/redactors_settings/interface/PresetsSelector.js
@@ -0,0 +1,442 @@
+import React, {useState, useRef, useEffect, memo} from "react";
+
+import {
+    StyleSheet, 
+    Text, 
+    Pressable, 
+    ScrollView,
+    FlatList, 
+    Animated, 
+    SectionList, 
+    View,
+    Button, 
+    Dimensions, 
+    Switch, 
+    Vibration,
+    ActivityIndicator
+} from 'react-native';
+
+import Reanimated, {
+    useSharedValue, 
+    useAnimatedStyle, 
+    withTiming,
+    interpolate,
+    useAnimatedProps,
+    cancelAnimation
+} from 'react-native-reanimated';
+
+import languagesAppList, { languagesApp } from "../../../../../../app_values/Languages";
+import themesColorsAppList, { themesApp } from "../../../../../../app_values/Themes";
+
+import { LinearGradient } from 'expo-linear-gradient';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const deviceHeight = Dimensions.get('window').height
+const deviceWidth = Dimensions.get('window').width
+
+//<MaterialCommunityIcons name="white-balance-sunny" size={24} color="black" />
+
+//<MaterialCommunityIcons name="weather-sunny" size={24} color="black" />
+//<MaterialCommunityIcons name="moon-waning-crescent" size={24} color="black" />
+//<MaterialCommunityIcons name="theme-light-dark" size={24} color="black" />
+
+import dataRedactor from "../../../../../../async_data_manager/data_redactor";
+
+import ColorShemeSwitch from "../../../../../../general_components/ColorShemeSwitch";
+
+import { 
+    BasePressable,
+    BaseBox,
+    BaseSlider,
+    BaseSwitch 
+} from "../../../../../../general_components/base_components/BaseElements";
+
+import commonStaticStyles, { BoxsField } from "../CommonElements";
+
+const Reanimated_Pressable = Reanimated.createAnimatedComponent(Pressable)
+
+const schemes = ['auto', 'light', 'dark'] 
+
+const presets = [
+    {
+        name: 'vk',
+        imageSource: '',
+    },
+    {
+        name: 'vk',
+        imageSource: '',
+    },
+    {
+        name: 'tg',
+        imageSource: '',
+    },
+    {
+        name: 'ig',
+        imageSource: '',
+    },
+    {
+        name: 'ws',
+        imageSource: '',
+    },
+]
+
+
+const ThemeItem = ({
+    title,
+
+    primaryCheck,
+    secondaryCheck,
+
+    onPress,
+    onLongPress,
+
+    pressDissable,
+
+    colors, //= {{
+        //gradient: [ThemeThisItem.basics.accents.primary, ThemeThisItem.basics.accents.quaternary],
+        //title: ThemeThisItem.texts.neutrals.primary,
+        //BackgroundColor: Theme.basics.neutrals.primary
+    //}}
+    itemSize,
+    outlineSize,
+
+    appStyle,
+},props) => {
+
+    const longPressed = useSharedValue(0)
+
+    const scaleStyle = useAnimatedStyle(()=>{
+        return ({
+                transform: [
+                {
+                    scale: interpolate(
+                        longPressed.value,
+                        [0, 1],
+                        [.95, .85]
+                    )
+                }
+            ]
+        }
+        )
+    })
+
+    const duration = 400
+
+    return (
+        <Reanimated_Pressable
+            key = {props.key}
+            disabled = {pressDissable} 
+            style={[{
+                height: itemSize,
+                width: itemSize,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }, scaleStyle]}
+            onPressIn={()=>{
+                cancelAnimation(longPressed)
+                longPressed.value = withTiming(1, {duration: duration})
+            }}
+            onPressOut={()=>{
+                cancelAnimation(longPressed)
+                longPressed.value = withTiming(0, {duration: duration/10})
+            }}
+            onPress={onPress}
+            delayLongPress={duration}
+            onLongPress={onLongPress}
+        >   
+            <LinearGradient
+                colors={colors.gradient}
+                start={{x: 0, y: 0}}
+                end={{x: 1, y: 1}}
+                locations={[.3,.6,.8,1]}
+                style={{
+                    position: 'absolute',
+                    height: itemSize,
+                    width: itemSize,
+                    borderRadius: appStyle.borderRadius.additional,
+                }}
+            />
+            {false && 
+            <Text 
+                style={[staticStyles.themeName, {
+                    color: colors.title,
+                    position: 'absolute',
+                }]}
+            >
+                {title}
+            </Text>}
+
+            <View
+                style={{
+                    height: itemSize-(2*outlineSize),//-(8)+1.5,
+                    width: itemSize-(2*outlineSize),
+                    position: 'absolute',
+                    alignItems: 'center',
+                    borderRadius: appStyle.borderRadius.additional -2,
+                    borderWidth:  outlineSize,
+                    borderColor: primaryCheck? colors.background: 'transparent',
+                }}
+            >
+                <View
+                    style={{
+                        height: itemSize/8,
+                        width: itemSize/3.1,
+                        position: 'absolute',
+                        top: -outlineSize,
+                        borderRadius: appStyle.borderRadius.additional,                       
+                        borderWidth:  secondaryCheck? outlineSize : 0,
+                        borderColor: colors.background,
+                        backgroundColor: secondaryCheck? 'transparent' : colors.background
+                    }}
+                />   
+            </View>
+         
+            <MaterialCommunityIcons 
+                name="format-color-text" 
+                size={25} 
+                color={colors.subTitle}
+                style={{
+                    position: 'absolute',
+                    top: (itemSize/2-12)+3,
+                    left: (itemSize/2-12)+3
+                }}
+            />
+            <MaterialCommunityIcons 
+                name="format-color-text" 
+                size={25} 
+                color={colors.title}
+                style={{
+                    position: 'absolute',
+                    bottom: (itemSize/2-12)+3,
+                    right: (itemSize/2-12)+3
+                }}
+            />
+        </Reanimated_Pressable>
+    )
+}
+
+export default Presets = ({
+    goToPalleteScreen,
+
+    appStyle,
+
+    previewAppStyleA,
+
+    //setAppStyle,
+    //r_setAppStyle,
+
+    ThemeColorsAppIndex,
+    ThemeSchema,
+    LanguageAppIndex  
+}) => {
+    const [previewThemeIndex, setPreviewThemeIndex] = useState(ThemeColorsAppIndex)
+    const [schema, setSchema] = useState(appStyle.palette.scheme)
+
+    const Theme = themesColorsAppList[ThemeColorsAppIndex][ThemeSchema]
+    const Language = languagesAppList[LanguageAppIndex].SettingsScreen.Redactors.themes
+
+    const flatListRef = useRef()
+    const scrollX = React.useRef(new Animated.Value(0)).current;
+    const [chosenThemeIndex, setChosenThemeIndex] = useState(themesApp.indexOf(appStyle.palette.theme))
+
+    const itemSize = 72
+    const outlineSize = 2
+
+    const pressItem = (index) => {
+        console.log('THEME PressItem', index)
+        if(previewThemeIndex != index){
+            changeThema(index)
+            flatListRef.current.scrollToIndex({index: index})
+        }
+    }
+
+    const longPressItem = (index) => {
+        Vibration.vibrate([5,10])
+        console.log('THEME longPressItem')    
+        if(index == 0 && themesColorsAppList[0]==null){
+            console.log('not custom theme for redactors')
+        } else {
+            goToPalleteScreen(index, 1)
+        }
+    }
+
+    
+
+    const changeThema = (themeIndex)=>{
+        const newAppStyle = JSON.parse(JSON.stringify(previewAppStyleA.value));
+        if(themeIndex != 0){
+            newAppStyle.palette.theme = themesApp[themeIndex]
+            cancelAnimation(previewAppStyleA)
+            previewAppStyleA.value = newAppStyle
+            setPreviewThemeIndex(themeIndex)
+        } else {
+            if(!themesColorsAppList[0]){
+                console.log('not custom theme')
+            } else {
+                console.log('custom theme ok')
+                newAppStyle.palette.theme = themesApp[themeIndex]
+                cancelAnimation(previewAppStyleA)
+                previewAppStyleA.value = newAppStyle
+                setPreviewThemeIndex(themeIndex)
+            }
+        }
+    }
+    
+
+    const createCustomTheme = ()=>{
+        console.log('custom theme create')
+        const newAppStyle = JSON.parse(JSON.stringify(previewAppStyleA.value));
+        const newTheme = JSON.parse(JSON.stringify(themesColorsAppList[ThemeColorsAppIndex]));
+        newTheme.light.theme = 'custom'
+        newTheme.dark.theme = 'custom'
+
+        newAppStyle.customTheme = newTheme
+        themesColorsAppList.splice(0,1,newTheme)
+
+        goToPalleteScreen(previewThemeIndex, 0)
+    }
+
+    const shemaSetting = (index) => {
+        const newAppStyle = JSON.parse(JSON.stringify(previewAppStyleA.value));
+        newAppStyle.palette.scheme = schemes[index]
+        cancelAnimation(previewAppStyleA)
+        previewAppStyleA.value = newAppStyle
+    }
+    
+
+    return (
+    <View
+        style={{
+        }}
+    >   
+        <BoxsField
+            //  'one'>true || 'multiple'>false
+            isChoiceOne={true}
+            title = {Language.colorMode}
+            //  'one'>index || 'multiple'>[indexs]
+            primaryValue = {schemes.indexOf(appStyle.palette.scheme)} 
+            groupSize = {schemes.length}
+            onPress = {(activeIndex)=>{shemaSetting(activeIndex)}}
+            groupItems = {Object.values(Language.colorsMods)}
+            appStyle = {appStyle}
+            ThemeColorsAppIndex = {ThemeColorsAppIndex}
+            ThemeSchema = {ThemeSchema}
+        />
+        <Text
+            style = {[staticStyles.text, {
+                color: Theme.texts.neutrals.secondary,
+                paddingLeft: 10,
+                marginTop: 10,
+            }]}
+        >
+            {Language.palette}
+        </Text> 
+        <View
+            style={{  
+                flex: 1, 
+                alignItems: "center"
+            }}
+        >
+        <FlatList
+            ref = {flatListRef}
+            style={{        
+                width: 5*itemSize,
+                height: itemSize,
+            }}
+            horizontal = {true}
+            showsHorizontalScrollIndicator = {false}
+            decelerationRate = {'fast'}
+            contentContainerStyle = {{
+                paddingRight: itemSize,
+            }}
+            snapToInterval={itemSize}
+            getItemLayout={(data, index) => (
+                {length: itemSize, offset: itemSize * index, index: index}
+            )}
+            initialScrollIndex = {ThemeColorsAppIndex}
+            data = {themesApp}         
+            keyExtractor={(item, index) => {
+                return item + index
+            }}
+            ListHeaderComponent={
+                <Pressable 
+                    style = {{
+                        width: itemSize,
+                        height: itemSize,
+
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: appStyle.borderRadius.additional,
+                        borderWidth:  outlineSize,
+                        borderColor: Theme.texts.neutrals.secondary,
+                        transform: [{scale: .95}],
+                        paddingTop: 6
+                    }}
+                    onPress={createCustomTheme}
+                >
+                    <MaterialCommunityIcons name="palette-advanced" size={35} color={Theme.texts.neutrals.secondary} />
+                    <Text
+                        style = {{
+                            top: -5,
+                            color: Theme.texts.neutrals.secondary,
+                            fontSize: 10,
+                            fontWeight: 'bold',
+                            letterSpacing: 0,
+                            fontVariant: ['small-caps'],
+                        }}
+                    >
+                        {Language.openPainter}
+                    </Text>
+                </Pressable>
+            }
+            renderItem={({item, index})=>{
+                const indexUsedTheme = themesApp.indexOf(appStyle.palette.theme)
+                const schemaThisItem = 'light'
+                const ThemeThisItem = themesColorsAppList[index]? themesColorsAppList[index][schemaThisItem] : Theme
+                const title = themesColorsAppList[index]? themesColorsAppList[index][schemaThisItem].theme : item
+                return (
+                <ThemeItem
+                    key = {String(`theme_selector_${item+index}`)} 
+                    title = {title}
+
+                    pressDissable = {themesColorsAppList[index]? false : true}
+                    
+                    onPress = {()=>{pressItem(index)}}
+                    onLongPress = {()=>{longPressItem(index)}}
+
+                    primaryCheck = {index === ThemeColorsAppIndex}
+                    secondaryCheck = {index === previewThemeIndex}//themesApp.indexOf(previewAppStyle.palette.theme)
+                   
+                    appStyle = {appStyle}
+                                    
+                    colors = {
+                        themesColorsAppList[index]? {
+                        gradient: [ThemeThisItem.basics.accents.primary,ThemeThisItem.basics.accents.secondary, ThemeThisItem.basics.accents.tertiary, ThemeThisItem.basics.accents.quaternary],
+                        title: ThemeThisItem.texts.neutrals.primary,
+                        background: Theme.basics.neutrals.secondary
+                        } : {
+                        //not custom palette
+                        gradient: ['#00000060','#00000040','#00000020','#0000000a'],
+                        subTitle: 'transparent', //,Theme.texts.neutrals.secondary,
+                        title: 'transparent',// Theme.texts.neutrals.primary,
+                        background: 'transparent'  
+                    }}
+                        
+                    itemSize={itemSize}
+                    outlineSize={outlineSize}
+                />
+            )}}
+        />
+        </View>
+    </View>)
+}
+
+const staticStyles = StyleSheet.create({
+    themeName: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        fontVariant: ['small-caps'],
+        //marginBottom: 10
+    },
+    ...commonStaticStyles
+});
